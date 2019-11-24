1b5ce07fdda9617c0218f75ac2d1130725a6d0c8
David <dmshayne@gmail.com>
2019-11-24 21:18:48 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,18 +1,33 @@
-// setup for links
-const home = document.getElementById("home");
+// setup for navigation links
+const home = document.querySelectorAll(".link-to-home");
 const about = document.getElementById("about");
 const services = document.getElementById("services");
 const contact = document.getElementById("contact");
-
-home.addEventListener("click", () => {
-    window.scroll({ top: 0, behavior: "smooth" });
+const contactCta = document.getElementById("cta");
+// setup for location elements
+const servicesLocation = document.querySelector(".services");
+const aboutLocation = document.querySelector(".about");
+const contactLocation = document.querySelector(".contact");
+const nav = document.querySelector("nav");
+// calculating current offset relative to viewport
+function offset(el) {
+    return el.getBoundingClientRect().top + window.scrollY - nav.clientHeight;
+}
+// auto-scrolling to location of element when link is clicked
+const setLink = (link, location) => {
+    link.addEventListener("click", () => {
+        window.scrollTo({
+            top: offset(location),
+            behavior: "smooth"
+        });
+    });
+};
+// setting the event listeners
+home.forEach(link => {
+    link.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
 });
-about.addEventListener("click", () => {
-    window.scroll({ top: 800, behavior: "smooth" });
-});
-services.addEventListener("click", () => {
-    window.scroll({ top: 1300, behavior: "smooth" });
-});
-contact.addEventListener("click", () => {
-    window.scroll({ top: document.body.clientHeight, behavior: "smooth" });
-});
+setLink(about, aboutLocation);
+setLink(services, servicesLocation);
+setLink(contact, contactLocation);
